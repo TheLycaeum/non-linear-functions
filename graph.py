cb1d8a21084c6e274b051758715ebf991362c39b
Noufal Ibrahim <noufal@nibrahim.net.in>
2014-12-22 23:41:56 +0530

    Initial commit

---
+++ graph.py
@@ -0,0 +1,159 @@
+import argparse
+import math
+import sys
+from math import *
+
+
+import pygame
+from pygame.locals import DOUBLEBUF, K_ESCAPE, KEYDOWN, QUIT
+
+
+def math2pygame(x, y):
+    scalex = SDL_XMAX/float(XMAX - XMIN)
+    scaley = SDL_YMAX/float(YMAX - YMIN)
+    gx = scalex*x + SDL_XMAX/2
+    gy = SDL_YMAX/2 - scaley*y
+    return gx, gy
+
+
+class Point(pygame.sprite.Sprite):
+    def __init__(self, x, y, colour, groups):
+        super(Point,self).__init__(groups)
+        self.image = pygame.Surface((1,1))
+        pygame.draw.circle(self.image, 
+                           (colour, colour, colour),
+                           (1,1),
+                           1,
+                           1)
+        self.rect = self.image.get_rect()
+        self.rect.center = math2pygame(x, y)
+        
+        # print "{:.04},{:.04} :: {:.04},{:.04}".format(x,y, *math2pygame(x, y))
+        self.c = colour
+    def update(self):
+        pass
+
+def draw_axes(surface, xmin, xmax, ymin, ymax):
+    s1, s2 = math2pygame(xmin, 0), math2pygame(0, ymin)
+    e1, e2 = math2pygame(xmax, 0), math2pygame(0, ymax)
+    pygame.draw.line(surface, (0, 100, 0), s1, e1, 1)
+    pygame.draw.line(surface, (0, 100, 0), s2, e2, 1)
+    pygame.draw.line(surface, (0, 255, 0), math2pygame(xmin, ymin), math2pygame(xmax, ymax), 1)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--winsize", dest = "winsize", default = "600x600", help = "Size of window")
+    parser.add_argument("--min", dest = "min", default = "-1x-1", help = "Min point of grid")
+    parser.add_argument("--max", dest = "max", default = "1x1", help = "Max point of grid")
+    parser.add_argument("--function", dest = "function", help = "Function to plot")
+    parser.add_argument("--start", dest = "start", action = "store", type = float, 
+                        help = "Start plotting from")
+    parser.add_argument("--stop", dest = "stop", action = "store", type = float, 
+                        help = "Stop plotting at")
+    parser.add_argument("--seed", dest = "seed", action = "store", type = float,
+                        help = "Point whose orbit to plot")
+    parser.add_argument("--iters", dest = "iters", action = "store", type = int, default = 0,
+                        help = "Number of iterations to plot")
+
+    args = parser.parse_args()
+    args.winsize = [int(x) for x in args.winsize.split("x")]
+    args.min = [float(x) for x in args.min.split("x")]
+    args.max = [float(x) for x in args.max.split("x")]
+    args.function = eval("lambda x : {}".format(args.function)) 
+    return args
+
+def graphical_plot(function, start, end, increment):
+    """Generates y values for given function starting from `start` and
+    ending at `end`. x values are incremented by increment.""" 
+    while start < end:
+        yield start, function(start)
+        start += increment
+    
+
+def graphical_analysis(seed, function, iterations):
+    for i in range(iterations):
+        t = function(seed)
+        start = seed, t
+        end = t, t
+        seed = t
+        yield start, end
+        
+        u = function(seed)
+        start = t, t
+        end = t, u
+        yield start, end
+
+    
+def main():
+    global SDL_XMAX
+    global SDL_YMAX
+    global XMIN
+    global YMIN
+    global XMAX
+    global YMAX
+
+    args = parse_args()
+    SDL_XMAX, SDL_YMAX = args.winsize
+    XMIN, YMIN = args.min
+    XMAX, YMAX = args.max
+
+    per_xpixel = float(XMAX - XMIN)/SDL_XMAX
+    per_ypixel = float(YMAX - YMIN)/SDL_YMAX
+
+
+    screen = pygame.display.set_mode((SDL_XMAX, SDL_YMAX), DOUBLEBUF)
+    screen.fill((0,10,0))
+    empty = pygame.Surface((SDL_XMAX, SDL_YMAX))
+    draw_axes(screen, XMIN, XMAX, YMIN, YMAX)
+    points = pygame.sprite.Group()
+    clock = pygame.time.Clock() 
+
+
+    for x, y in graphical_plot(args.function, args.start, args.stop, per_xpixel):
+        Point(x, y, 255, points)
+
+    lines = graphical_analysis(args.seed, args.function, args.iters)
+    
+    col = 20
+    while True:
+        clock.tick(30)
+
+        if args.iters:
+            try:
+                start,stop = next(lines)
+                if start < stop:
+                    start, stop = stop, start
+                pygame.draw.line(screen, (100, 0, col), math2pygame(*start), math2pygame(*stop), 2)
+                print stop[1]
+                start,stop = next(lines)
+                if start < stop:
+                    start, stop = stop, start
+
+                pygame.draw.line(screen, (100, 0, col), math2pygame(*start), math2pygame(*stop), 2)
+
+            except StopIteration:
+                pass
+            except TypeError:
+                pass
+            # except OverflowError:
+            #     pass
+
+            col = (col + 50)%255
+
+
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                sys.exit()
+
+        # Update sprites
+        points.clear(screen, empty)
+        points.update()
+        points.draw(screen)
+        pygame.display.flip()
+
+
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ linear_plot.py
@@ -0,0 +1,151 @@
+import argparse
+import math
+import sys
+from math import *
+
+
+import pygame
+from pygame.locals import DOUBLEBUF, K_ESCAPE, KEYDOWN, QUIT
+
+
+SDL_XMAX, SDL_YMAX = 700, 700
+XMIN, YMIN, XMAX, YMAX = -100, -100, 100, 100
+
+def math2pygame(x, y):
+    scalex = SDL_XMAX/float(XMAX - XMIN)
+    scaley = SDL_YMAX/float(YMAX - YMIN)
+    gx = scalex*x + SDL_XMAX/2
+    gy = SDL_YMAX/2 - scaley*y
+    return gx, gy
+
+
+class Panel(pygame.sprite.Sprite):
+    def __init__(self, val, groups):
+        super(Panel, self).__init__(groups)
+        self.font = pygame.font.Font("jGara2.ttf", 20)
+        self.image = pygame.Surface((100,25)).convert_alpha()
+        self.image.fill((0,10,0))
+        self.rect = self.image.get_rect()
+        self.rect.bottomright = (SDL_XMAX, SDL_YMAX)
+        self.val = val
+        info = self.font.render("({},{})".format(val[0], val[1]), True,(20,200,20))
+        self.image.fill((0,10,0,0))
+        self.image.blit(info,(0,0))
+
+    def update(self):
+        info = self.font.render("({}  ,  {})".format(self.val[0], self.val[1]), True,(20,200,20))
+        self.image.fill((0,10,0,0))
+        self.image.blit(info,(0,0))
+
+
+
+
+class Point(pygame.sprite.Sprite):
+    def __init__(self, x, y, colour, dies_in, panel, groups):
+        super(Point,self).__init__(groups)
+        self.image = pygame.Surface((8,8))
+        pygame.draw.circle(self.image, 
+                           (colour, colour, colour),
+                           (4,4),
+                           4,
+                           0)
+        self.rect = self.image.get_rect()
+        self.rect.center = math2pygame(x, y)
+        self.life = dies_in
+        self.c = colour
+        self.dec = colour/dies_in
+        self.panel = panel
+        self.x, self.y = x, y
+
+    def update(self):
+        self.life -= 1
+        self.c -= self.dec
+        self.c = int(self.c)
+        pygame.draw.circle(self.image, (self.c, self.c, self.c), (4,4), 4, 0)
+        x, y = pygame.mouse.get_pos()
+        x0, y0, x1, y1 = self.rect
+        if x0 < x < x0+x1 and y0 < y < y0+y1:
+            self.panel.val = ["{:.3}".format(float(self.x)), "{:.3}".format(float(self.y))]
+        if not self.life:
+            self.kill()
+            
+def draw_axes(surface, interval):
+    s1, s2 = math2pygame(-100, 0), math2pygame(0, -100)
+    e1, e2 = math2pygame(100, 0), math2pygame(0, 100)
+    pygame.draw.line(surface, (0, 200, 0), s1, e1, 1)
+    pygame.draw.line(surface, (0, 200, 0), s2, e2, 1)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--winsize", dest = "winsize", default = "600x600", help = "Size of window")
+    parser.add_argument("--min", dest = "min", default = "-1x-1", help = "Min point of grid")
+    parser.add_argument("--max", dest = "max", default = "1x1", help = "Max point of grid")
+    parser.add_argument("--function", dest = "function", help = "Function to plot")
+    parser.add_argument("--start", dest = "start", action = "store", type = float, 
+                        help = "Number whose orbit to plot")
+    args = parser.parse_args()
+    args.winsize = [int(x) for x in args.winsize.split("x")]
+    args.min = [int(x) for x in args.min.split("x")]
+    args.max = [int(x) for x in args.max.split("x")]
+    if args.function in globals():
+        args.function = globals()[args.function]
+    else:
+        args.function = eval("lambda x : {}".format(args.function)) 
+    return args
+
+def linear_plot(function, start, iters = False):
+    "Generates orbit of `start` with `function`. Stops  at `iters`"
+    if iters:
+        for i in range(iters):
+            start = function(start)
+            yield start
+    else:
+        while True:
+            start = function(start)
+            yield start
+    
+def main():
+    global SDL_XMAX
+    global SDL_YMAX
+    global XMIN
+    global YMIN
+    global XMAX
+    global YMAX
+
+    args = parse_args()
+    SDL_XMAX, SDL_YMAX = args.winsize
+    XMIN, YMIN = args.min
+    XMAX, YMAX = args.max
+
+    screen = pygame.display.set_mode((SDL_XMAX, SDL_YMAX), DOUBLEBUF)
+    screen.fill((0,10,0))
+    pygame.font.init()
+    empty = pygame.Surface((SDL_XMAX, SDL_YMAX))
+    draw_axes(screen, 1)
+    points = pygame.sprite.Group()
+    clock = pygame.time.Clock() 
+    panel = Panel(("",""), points)
+
+    orbit = linear_plot(args.function, args.start)
+    
+    while True:
+        clock.tick(30)
+
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                sys.exit()
+
+        Point(next(orbit), 0, 100, 100, panel, points)
+
+        # Update sprites
+        points.clear(screen, empty)
+        points.update()
+        points.draw(screen)
+        pygame.display.flip()
+
+
+
+
+if __name__ == '__main__':
+    main()
+
